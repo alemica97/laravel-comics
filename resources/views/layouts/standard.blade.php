db7cff3fdb72552f30d8c58986ef73a6f842d422
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-19 16:52:59 +0200

    ho creato la struttura base del layout

---
+++ resources/views/layouts/standard.blade.php
@@ -0,0 +1,21 @@
+<!-- Creo il layout da poter riutilizzare nelle varie pagine  -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- includeo l'header che si trova nella cartella partials  -->
+    @include('partials.header')
+
+    <main>
+
+    </main>
+
+    <!-- includo il footer che si trova nella cartella partials  -->
+    @include('partials.footer')
+</body>
+</html>
