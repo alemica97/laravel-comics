09d4b198380da02144c6c3497900c6ea886ee0e4
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-20 17:56:38 +0200

    ho aggiunto l'elemento superiore al footer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,35 @@
+@php
+
+$merch_link_array = [
+                [
+                  "image" => asset('img/buy-comics-digital-comics.png'),
+                  "href" => '#',
+                  "text" => 'digital comics',
+                ],
+                [
+                  "image" => asset('img/buy-comics-merchandise.png'),
+                  "href" => '#',
+                  "text" => 'dc merchandise',
+                ],
+                [
+                  "image" => asset('img/buy-comics-subscriptions.png'),
+                  "href" => '#',
+                  "text" => 'subscription',
+                ],
+                [
+                  "image" => asset('img/buy-comics-shop-locator.png'),
+                  "href" => '#',
+                  "text" => 'comic shop locator',
+                ],
+                [
+                  "image" => asset('img/buy-dc-power-visa.svg'),
+                  "href" => '#',
+                  "text" => 'dc power visa',
+                ],
+];
+
+@endphp
+
 <!-- estendo il layout standard alla pagina home  -->
 @extends('layouts.standard')
 
@@ -27,8 +59,24 @@
             </div>
             <button class="load-btn">load more</button>
         </section>
+
+        <section>
+            <div class="footer-header">
+                <div class="container">
+                  <ul>
+                        @foreach($merch_link_array as $el)
+                        <li class="merch-link">
+                        <figure>
+                            <img src="{{ $el['image'] }}" alt="">
+                        </figure>
+                        <a href="{{ $el['href'] }}">{{ $el['text'] }}</a>
+                        </li>
+                        @endforeach
+                  </ul>
+                </div>
+              </div>
+        </section>
         
     </main>
-    jumbotron <br>
-    elenco delle card
+
 @endsection
