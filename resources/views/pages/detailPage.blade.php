3a632bcf05e4c4057ef42c78a0e6c8ecd4fdf30f
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-20 20:05:27 +0200

    completata la parte centrale del main, della pagina specifica

--- resources/views/pages/detailPage.blade.php
+++ resources/views/pages/detailPage.blade.php
@@ -15,6 +15,31 @@
                 </figure>
             </div>
         </section>
+        <section class="container-medium description">
+            <div class="movie-info">
+                <div class="title">
+                    <h1>{{ $card['title'] }}</h1>
+                </div>
+                <div class="green-info">
+                    <div class="gi-left">
+                        <span>U.S. Price:</span><span>{{ $card['price'] }}</span>
+                        <span>Available</span>
+                    </div>
+                    <div class="gi-right">
+                        <span>Check Availability<i class="fa-solid fa-caret-down"></i></span>
+                    </div>
+                </div>
+                <div class="movie-description">
+                    <p>{{ $card['description'] }}</p>
+                </div>
+            </div>
+            <div class="advertisement">
+                <span>advertisement</span>
+                <figure class="thumb-image">
+                    <img src="{{ asset('img/adv.jpg') }}" alt="">
+                </figure>
+            </div>
+        </section>
     </main>
     
 @endsection
