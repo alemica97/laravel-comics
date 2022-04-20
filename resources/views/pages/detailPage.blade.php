11bdf633c0b8d81283420ab4cd52553cb0775090
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-20 21:50:06 +0200

    ho finito la pagina specifica, anche se c'è un errore nella parte bassa, non ho capito come vanno inserite le immagini singolarmente.

--- resources/views/pages/detailPage.blade.php
+++ resources/views/pages/detailPage.blade.php
@@ -1,3 +1,26 @@
+@php
+
+    $logo_array = [
+        [
+            "title" => 'DIGITAL COMICS',
+            "image" => asset('img/cta-icons.png')
+        ],
+        [
+            "title" => 'SHOP DC',
+            "image" => asset('img/cta-icons.png')
+        ],
+        [
+            "title" => 'COMIC SHOP LOCATOR',
+            "image" => asset('img/cta-icons.png')
+        ],
+        [
+            "title" => 'SUBSCRIPTIONS',
+            "image" => asset('img/cta-icons.png')
+        ]
+    ];
+
+@endphp
+
 @extends('layouts.standard')
 
 @section('titolo',$card['title'])
@@ -15,29 +38,100 @@
                 </figure>
             </div>
         </section>
-        <section class="container-medium description">
-            <div class="movie-info">
-                <div class="title">
-                    <h1>{{ $card['title'] }}</h1>
-                </div>
-                <div class="green-info">
-                    <div class="gi-left">
-                        <span>U.S. Price:</span><span>{{ $card['price'] }}</span>
-                        <span>Available</span>
+
+        <section>
+            <div class="container-medium description">
+                <div class="movie-info">
+                    <div class="title">
+                        <h1>{{ $card['title'] }}</h1>
                     </div>
-                    <div class="gi-right">
-                        <span>Check Availability<i class="fa-solid fa-caret-down"></i></span>
+                    <div class="green-info">
+                        <div class="gi-left">
+                            <span>U.S. Price:</span><span>{{ $card['price'] }}</span>
+                            <span>Available</span>
+                        </div>
+                        <div class="gi-right">
+                            <span>Check Availability<i class="fa-solid fa-caret-down"></i></span>
+                        </div>
+                    </div>
+                    <div class="movie-description">
+                        <p>{{ $card['description'] }}</p>
                     </div>
                 </div>
-                <div class="movie-description">
-                    <p>{{ $card['description'] }}</p>
+                <div class="advertisement">
+                    <span>advertisement</span>
+                    <figure class="thumb-image">
+                        <img src="{{ asset('img/adv.jpg') }}" alt="">
+                    </figure>
                 </div>
             </div>
-            <div class="advertisement">
-                <span>advertisement</span>
-                <figure class="thumb-image">
-                    <img src="{{ asset('img/adv.jpg') }}" alt="">
-                </figure>
+        </section>
+
+        <section class="talent-specs-section">
+            <div class="container-medium detail-talent">
+                <div class="talent">
+                    <h3>Talent</h3>
+                    <div class="detailed-info">
+                        <div class="col-4">
+                            <span>Art by:</span>
+                        </div>
+                        <div class="col-8">
+                            @foreach($card['artists'] as $artist)
+                            <span>{{ $artist }},</span>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="detailed-info">
+                        <div class="col-4">
+                            <span>Written by:</span>
+                        </div>
+                        <div class="col-8">
+                            @foreach($card['writers'] as $writer)
+                            <span>{{ $writer }},</span>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="specs">
+                    <h3>Specs</h3>
+                    <div class="detailed-info">
+                        <div class="col-4">
+                            <span>Series:</span>
+                        </div>
+                        <div class="col-8">
+                            <span>{{ $card['series'] }}</span>
+                        </div>
+                    </div>
+                    <div class="detailed-info">
+                        <div class="col-4">
+                            <span>U.S. Price:</span>
+                        </div>
+                        <div class="col-8">
+                            <span>{{ $card['price'] }}</span>
+                        </div>
+                    </div>
+                    <div class="detailed-info">
+                        <div class="col-4">
+                            <span>On Sale Date:</span>
+                        </div>
+                        <div class="col-8">
+                            <span>{{ $card['sale_date'] }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="logos-section">
+            <div class="container-medium logos">
+                @foreach($logo_array as $logo)
+                <div class="col-3">
+                    <span>{{ $logo['title'] }}</span>
+                    <figure>
+                        <img src="{{ $logo['image'] }}" alt="">
+                    </figure>
+                </div>
+                @endforeach
             </div>
         </section>
     </main>
